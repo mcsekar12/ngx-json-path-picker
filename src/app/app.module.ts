0b584e082903487e4ada11cdd4f080a5f2c068a9
mcsekar12 <chandrasekar.m12@gmail.com>
2018-10-25 15:35:40 +0530

    config issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { JsonPathPickerModule } from 'json-path-picker';
 import { FormsModule } from '@angular/forms';
+import { JsonPathPickerModule } from 'projects/ngx-json-path-picker/src/public_api';
 
 @NgModule({
   declarations: [AppComponent],
